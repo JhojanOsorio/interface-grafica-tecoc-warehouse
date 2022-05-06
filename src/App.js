5f4bb3582abde529ebfabe2c4c86ee25ceff8184
Jhojan Osorio <jhojanxd125@gmail.com>
2022-05-06 09:23:29 -0500

    Sidebar listo

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 
+
 import Products from './pages/products';
 import Clients from './pages/clients';
 import Responsible from './pages/responsible';
 import Users from './pages/users';
 import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
-
+import Admin from './assets/img/Admin.jpeg'
 function App() {
   return (
     <BrowserRouter>
     
     <Router>
    
-     <Navbar />
+     
      <div className="flex">   
+     
        <Sidebar/>
-       <div className="content">
+       <div className="content w-100">
+         
+       <Navbar />
       <Route path ="/" element={<Products></Products>} />
       <Route path ="/clients" element={<Clients></Clients>}/>
       <Route path ="/responsible" element={<Responsible></Responsible>}/>
